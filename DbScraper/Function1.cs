85f2743a45b9a9b3630a45b0edbfa587c4114f4d
ty4mcq <ty4mcq.2101@hotmail.com>
2024-10-09 17:15:55 +0100

    Added new SQL connection + example query

--- DbScraper/Function1.cs
+++ DbScraper/Function1.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
 namespace DbScraper
@@ -14,13 +15,36 @@ namespace DbScraper
         }
 
         [Function("Function1")]
-        public void Run([TimerTrigger("0 0 1 */3 *")] TimerInfo myTimer)
+        public void Run([TimerTrigger("* * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             
             if (myTimer.ScheduleStatus is not null)
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+
+            Console.WriteLine("This has temporarily been changed from running once every 3 months to every minute for testing purposes.");
+
+            //Starting new SQL connection
+            using(var connection = new SqlConnection())
+            {
+                //Retreiving connection string from environment variables in local.settings.json
+                connection.ConnectionString = Environment.GetEnvironmentVariable("productionDb");
+                connection.Open();
+
+                Console.WriteLine("Success");
+
+                //Defining query and executing
+                var query = "SELECT * FROM Records";
+                var command = new SqlCommand(query, connection);
+                var reader = command.ExecuteReader();
+
+                //Outputting read data
+                while (reader.Read())
+                {
+                    _logger.LogInformation($"Production Record: {reader["Data"]}, Created: {reader["CreatedDate"]}");
+                }
             }
         }
     }
