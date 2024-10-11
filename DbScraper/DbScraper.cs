31578c9b77098cad90ea97df64e3a0962699d2c4
ty4mcq <ty4mcq.2101@hotmail.com>
2024-10-11 13:36:58 +0100

    Registered DbContext in Program.cs + Added oldRecords LINQ Query

--- DbScraper/DbScraper.cs
+++ DbScraper/DbScraper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing.Printing;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
@@ -8,46 +9,40 @@ namespace DbScraper
     public class DbScraper
     {
         private readonly ILogger _logger;
+        private readonly ProductionDbContext _productionDbContext;
+        private readonly ArchiveDbContext _archiveDbContext;
 
-        public DbScraper(ILoggerFactory loggerFactory)
+        public DbScraper(ILoggerFactory loggerFactory, ProductionDbContext productionDbContext, ArchiveDbContext archiveDbContext)
         {
             _logger = loggerFactory.CreateLogger<DbScraper>();
+            _productionDbContext = productionDbContext;
+            _archiveDbContext = archiveDbContext;
         }
 
         [Function("DbScraper")]
         public void Run([TimerTrigger("* * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            
+
             if (myTimer.ScheduleStatus is not null)
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             }
 
-            Console.WriteLine("This has temporarily been changed from running once every 3 months to every minute for testing purposes.");
+            Console.WriteLine("This has temporarily been changed from running once every week to every minute for testing purposes.");
 
-            //Starting new SQL connection
-            using(var productionConn = new SqlConnection())
+            var oldRecords = _productionDbContext.Records.Where(r => r.CreatedDate < DateTime.Now.AddMonths(-3)).OrderBy(r => r.Id).ToList();
+            _logger.LogInformation($"Found {oldRecords.Count} old records. Here are the old records.");
+
+            foreach (var record in oldRecords)
             {
-                //Retreiving productionuction connection string from environment variables in local.settings.json
-                productionConn.ConnectionString = Environment.GetEnvironmentVariable("productionDb");
-                productionConn.Open();
+                _logger.LogInformation($"Old record: {record.Id} - {record.Data} - {record.CreatedDate}");
+            }
 
-                Console.WriteLine("Success");
-
-                //Defining scrape query to find records older than 3 months and executing query
-                var scrapeQuery = @"SELECT *
-                FROM Records
-                WHERE CreatedDate < DATEADD(month, -3, GETDATE())";
-
-                var scrapeCmd = new SqlCommand(scrapeQuery, productionConn);
-                var scrapeReader = scrapeCmd.ExecuteReader();
-
-                //Outputting read data
-                while (scrapeReader.Read())
-                {
-                    _logger.LogInformation($"productionuction Record: {scrapeReader["Data"]}, Created: {scrapeReader["CreatedDate"]}");
-                }
+            var allRecords = _productionDbContext.Records.OrderBy(r => r.Id).ToList();
+            foreach (var record in allRecords)
+            {
+                _logger.LogInformation($"Record: {record.Id} - {record.Data} - {record.CreatedDate}");
             }
         }
     }
--- DbScraper/Program.cs
+++ DbScraper/Program.cs
@@ -1,4 +1,6 @@
+using DbScraper;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +10,9 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddDbContext<ProductionDbContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("productionDb")));
+        services.AddDbContext<ArchiveDbContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("archiveDb")));
+
     })
     .Build();
 
